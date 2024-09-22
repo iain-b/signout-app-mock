b4d354f3c6b64777e4a68b546512f526ab51d413
Iain Billett <iain@roadie.io>
2024-09-22 17:25:09 +0100

    Initial CRA app with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Add A&E Admission
+                </p>
+                <label htmlFor="admitting-diagnosis">Addmitting Diagnosis</label>
+                <input id="admitting-diagnosis" />
+            </header>
+        </div>
+    );
 }
 
 export default App;
