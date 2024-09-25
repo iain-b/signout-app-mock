ebcd282eed2af5ec5d3fac6d43be2af726d69758
Iain Billett <iain@roadie.io>
2024-09-25 10:07:25 +0100

    WIP

--- src/AEAdmissionForm.tsx
+++ src/AEAdmissionForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import { Autocomplete, Button, Input, TextField } from "@mui/material";
+import { useForm, SubmitHandler, Controller, useWatch } from "react-hook-form";
+import { Autocomplete, Button, Input, Switch, TextField } from "@mui/material";
 
 type Inputs = {
   admittingDiagnosis: string;
@@ -9,6 +9,14 @@ type Inputs = {
   location: string;
   dateOfBirth: string;
   background: string[];
+  hasAnticoagulant: boolean;
+  anticoagulantUsed?: string;
+  hasAntiplatelet: boolean;
+  antiplateletUsed?: string;
+  oe: string;
+  imaging: string;
+  isImagingFinalised: boolean;
+  plan: string;
 };
 
 export const AEAdmissionForm = () => {
@@ -22,6 +30,11 @@ export const AEAdmissionForm = () => {
       background: [],
     },
   });
+  const [hasAnticoagulant, hasAntiplatelet] = useWatch({
+    control,
+    name: ["hasAnticoagulant", "hasAntiplatelet"],
+  });
+  console.log("watches", hasAnticoagulant);
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
@@ -139,6 +152,116 @@ export const AEAdmissionForm = () => {
               />
             )}
           />
+        </div>
+        <div className="col-span-3">
+          <label htmlFor="location">Anticoagulant</label>
+        </div>
+        <div className="col-span-3">
+          <Switch {...register("hasAnticoagulant")} />
+        </div>
+        <div className="col-span-6">
+          <Controller
+            name="anticoagulantUsed"
+            control={control}
+            disabled={!hasAnticoagulant}
+            render={({ field: { onChange, value } }) => (
+              <Autocomplete
+                disabled={!hasAnticoagulant}
+                freeSolo
+                options={[
+                  "Apixaban",
+                  "Rivaroxaban",
+                  "Edoxaban",
+                  "Dabigatran",
+                  "Warfarin",
+                  "Other (freetext)",
+                ]}
+                value={value}
+                onChange={(event, newValue) => onChange(newValue)}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Select or type"
+                    variant="outlined"
+                    size="small"
+                  />
+                )}
+              />
+            )}
+          />
+        </div>
+
+        <div className="col-span-3">
+          <label htmlFor="location">Antiplatelet</label>
+        </div>
+        <div className="col-span-3">
+          <Switch {...register("hasAntiplatelet")} />
+        </div>
+        <div className="col-span-6">
+          <Controller
+            name="antiplateletUsed"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Autocomplete
+                freeSolo
+                options={["Aspirin", "Clopidogrel", "Prasugrel", "Ticagrelor"]}
+                value={value}
+                disabled={!hasAntiplatelet}
+                onChange={(event, newValue) => onChange(newValue)}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Select or type"
+                    variant="outlined"
+                    size="small"
+                  />
+                )}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-3">
+          <label htmlFor="location">O/E</label>
+        </div>
+        <div className="col-span-9">
+          <TextField
+            className="w-full"
+            size="small"
+            variant="outlined"
+            {...register("oe")}
+          />
+        </div>
+        <div className="col-span-3">
+          <label htmlFor="location">Imaging</label>
+        </div>
+        <div className="col-span-3 pr-4">
+          <Controller
+            name="imaging"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Autocomplete
+                options={["CT", "US", "Other", "Awaited", "None planned"]}
+                value={value}
+                onChange={(event, newValue) => onChange(newValue)}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Select or type"
+                    variant="outlined"
+                    size="small"
+                  />
+                )}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-3">
+          <label className="pl-1" htmlFor="isImagingFinalised">
+            Finalised Report
+          </label>
+        </div>
+        <div className="col-span-3">
+          <Switch {...register("isImagingFinalised")} />
         </div>
         <div className="col-span-12">
           <Button type="submit" variant="contained" color="primary">
